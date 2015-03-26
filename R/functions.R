75745ad17c280bd7a121166acdc46b3300dd64cb
Colin Gillespie <csgillespie@gmail.com>
2015-03-26 22:14:20 +0000

    Added a function to make the package a bit more realistic.

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+#' A simple adding functions
+#'
+#' An example adding function
+#' @param x a number
+#' @param y another number
+#' @examples
+#' add(1, 2)
+#' @export
+add = function(x, y){
+  x + y
+}
